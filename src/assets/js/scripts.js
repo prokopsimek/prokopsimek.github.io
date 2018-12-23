edc3fb741d2219f603efcdc846a350aa60860c35
Prokop Simek <gilotka@gmail.com>
2018-12-23 20:16:06 +0100

    added scripts support

---
+++ src/assets/js/scripts.js
@@ -0,0 +1,15 @@
+---
+layout: null
+---
+
+alert("scripts loaded!");
+
+
+$(document).ready(function() { 
+  $("h1").addClass("javaScriptAddedClass1");
+
+ });
+$(function() { 
+  $("h1").addClass("javaScriptAddedClass2");
+
+ });
